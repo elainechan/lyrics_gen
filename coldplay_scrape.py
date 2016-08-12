caa71c502e1d09547b9a0be57e9371ab76160b6c
Elaine Chan <elainechan@users.noreply.github.com>
2016-08-12 00:23:39 -0400

    A test Web scraper that collects lyrics.

---
+++ coldplay_scrape.py
@@ -0,0 +1,32 @@
+'''
+coldplay.py
+A Web scraper that collects a corpus of Coldplay lyrics.
+
+Functions used:
+BeautifulSoup.findAll(tag, attributes, recursive, text, limit, keywords)
+BeautifulSoup.find(tag, attributes, recursive, text, keywords)
+'''
+
+from urllib.request import urlopen 
+from bs4 import BeautifulSoup
+import re
+
+# scrape website for URLs
+html = urlopen("http://www.azlyrics.com/c/coldplay.html") 
+bsObj = BeautifulSoup(html)
+for link in bsObj.findAll("a",
+	href=re.compile("^(/lyrics/coldplay/)((?!:).)*$")):
+	if 'href' in link.attrs: 
+		print(link.attrs['href'])
+
+# make list of URLs
+url_list = []
+
+# traverse through list of URLs
+def traverse_links(url_list):
+	for link in url_list:
+		html = urlopen("link")
+
+# locate specific elements on page with each URL
+
+# collect text in elements and write into one single text file
