469a7140a59472b911dc32727823c4f57047e026
Elaine Chan <elaine.chan@outlook.com>
2016-08-15 15:21:16 -0400

    changed urllib methods

--- coldplay_scrape.py
+++ coldplay_scrape.py
@@ -16,20 +16,13 @@ def get_lyrics_links(artist_url, local_filename):
 	soup = BeautifulSoup(html, 'html.parser')
 	return soup.prettify()
 	print(soup.prettify())
-	# locations = soup.findAll(id='listAlbum')
-	# return locations
-	# print(len(locations))
 
-get_lyrics_links('http://www.azlyrics.com/c/coldplay.html', 'url_resource.html')
+# get_lyrics_links('http://www.azlyrics.com/c/coldplay.html', 'url_resource.html')
 
-# # make list of URLs
-# url_list = []
+# make list of URLs
 
-# # traverse through list of URLs
-# def traverse_links(url_list):
-# 	for link in url_list:
-# 		html = urlopen("link")
+# traverse through list of URLs
 
-# # locate specific elements on page with each URL
+# locate specific elements on page with each URL
 
-# # collect text in elements and write into one single text file
+# collect text in elements and write into one single text file
